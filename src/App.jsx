f84398d1b1ae0130213d69f7a68784b4f2e3840b
Oscar Jimenez <oscar.jimenez@wundermanthompson.com>
2024-07-25 20:26:48 +0200

    Add & Remove products to the Cart

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import { products as initialProducts } from './mocks/products.json';
+import { Cart } from './components/Cart'
 import { Footer } from './components/Footer';
 import { Header } from './components/Header';
 import { Products } from './components/Products';
+import { CartProvider } from './contexts/cart'
 import { useFilters } from './hooks/useFilters';
 
 import { IS_DEVELOPMENT } from './config';
@@ -12,11 +14,12 @@ function App() {
     const filteredProducts = filterProducts(initialProducts);
 
     return (
-        <>
+        <CartProvider>
             <Header />
+            <Cart />
             <Products products={filteredProducts} />
             {IS_DEVELOPMENT && <Footer />}
-        </>
+        </CartProvider>
     );
 }
 
